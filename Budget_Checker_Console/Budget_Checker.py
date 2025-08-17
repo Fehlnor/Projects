d933803023bcc1ae99a7aa483ff58f05662445a9
Fehér Norbert <feher.norbert165@gmail.com>
2025-08-17 14:28:37 +0200

    Create Budget_Checker.py
    This is a console application. It will check with a certain budget if you can afford what is in the cart or not.
    -Check how much you buy from a certain product
    -Calculate the amount of money
    -Decide if affordable or not

---
+++ Budget_Checker_Console/Budget_Checker.py
@@ -0,0 +1,9 @@
+# You have 250 euro, you go shopping and you can buy 
+# flour (8euro / kg)
+# honey (22euro / kg)
+# sugar (6euro / kg)
+# milk (4euro / l)
+# bread (15euro / kg) 
+# water (12euro / l)
+# The program should read how much you want to buy of each item and then print the total amount to pay. 
+# If the total exceeds the amount of money you have, the program should print that.
