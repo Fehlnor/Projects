5fb394bb21147f941f9a91445e522f7e4e66ac88
Fehér Norbert <feher.norbert165@gmail.com>
2025-09-01 10:46:12 +0200

    Create Number_Guesser.py
    This is a console application. It will create a random number and ask the user to guess it.
    -Ask the user for the number and amount of guesses
    -Have 3 levels of difficulty
    -Say if the number is higher or less
    -Create a statistics of the amount of guesses

---
+++ Number_Guessing_Console/Number_Guesser.py
@@ -0,0 +1,5 @@
+# Simple number guesser, the program creates a random number
+# The program ask the player for guesses & how many times the player can guess the number
+# Create a statistics of how many guess was there
+# Have a difficulty level
+# Say if the number is higher or less
